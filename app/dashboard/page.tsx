5b9a537e0b4a8be38ba7b01874c7acb61185b7ea
amankidamtea <idamassegaf9@gmail.com>
2024-05-30 15:54:21 +0700

    update

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function page(){
+    return(
+        <p>Page Dashboard</p>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Playfair_Display} from "next/font/google"
+
+export const inter = Inter({subsets: ['latin']})
+export const play = Playfair_Display({subsets: ['latin']})
